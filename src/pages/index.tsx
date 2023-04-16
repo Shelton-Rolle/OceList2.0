0bc1b2ea275687c983e05ae0a24b2a51dad82f7d
SheltonRolle_Mac <sheltonrolle7@gmail.com>
2023-04-16 13:00:34 -0400

    added the getServerSideProps funciton to the home page to display highlighted projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import Head from 'next/head';
 import RootLayout from '@/layouts/RootLayout';
-import { BsTwitter } from 'react-icons/bs';
-import { MdOutlineEmail } from 'react-icons/md';
+import { GetServerSideProps } from 'next';
+import database from '@/firebase/rt_database/init';
+import { get, ref } from 'firebase/database';
+interface HomePageProps {
+    highlightedProjects: any[];
+}
 
-export default function Home() {
+export default function Home({ highlightedProjects }: HomePageProps) {
+    console.log('Highlighted Projects: ', highlightedProjects);
     return (
         <>
             <Head>
@@ -18,62 +23,43 @@ export default function Home() {
                 />
             </Head>
             <RootLayout>
-                <section id="welcome" className="mb-32 pt-20">
-                    <div id="heading" className="mb-6">
-                        <h1 className="font-roboto font-bold uppercase text-xl">
-                            Welcome To
-                        </h1>
-                        <h2 className="font-poppins font-bold text-5xl">
-                            OceList
-                        </h2>
-                    </div>
-                    <p className="font-poppins-light text-base w-3/4 leading-8">
-                        OceList is your new one-stop shop for Open Source
-                        projects! We provide a wide range of open source
-                        projects of all kinds for you to view and start
-                        collaborating on, as well as provide a free platform for
-                        developers to list their very own open source projects
-                        and get new collaborators! Start browsing now!
+                <section id="hero">
+                    <h1>Welcome To</h1>
+                    <h2>OceList</h2>
+                    <p>
+                        OceList is a platform with one purpose, to provide
+                        developers with an easier way of discovering Open Source
+                        repositories to contribute to. OceList has an ever
+                        growing collection of Open Source repositories ranging
+                        from ones owned by huge corporations, to smaller
+                        repositories created by solo developers. OceList gives
+                        you a place where you can easily find projects that fit
+                        your interests and start contributing.
                     </p>
                 </section>
-                <section
-                    id="why-ocelist"
-                    className="relative -z-10 mb-32 py-20 text-white"
-                >
-                    <h3 className="font-poppins font-bold text-md mb-3 text-[40px]">
-                        Why OceList?
-                    </h3>
-                    <p className="font-poppins text-base leading-8 w-3/4">
-                        OceList is constantly being updated with new projects by
-                        myself, the creator. With this, there will always be a
-                        reliable source of popular open source projects for
-                        users to select from.
-                    </p>
+                <section id="highlighted-projects">
+                    {/* Display a list of a few highlighted projects */}
                 </section>
-                <section id="find-contributors">
-                    <h4 className="font-poppins font-bold text-md mb-3 text-[40px]">
-                        Looking For Contributors?
-                    </h4>
-                    <p className="font-poppins text-sm leading-7 lg:text-base lg:leading-8 lg:w-3/4">
-                        If you&apos;re looking for contributors for your Open
-                        Source project, feel free to contact me with regards to
-                        getting your project listed here on OceList! It&apos;s
-                        completely free.
-                    </p>
+                <section id="recommendations">
                     <div>
-                        <h5 className="mb-3 mt-6 font-roboto text-lg font-bold">
-                            Contact Information
-                        </h5>
-                        <div className="font-light flex flex-col gap-2">
-                            <a
-                                href="https://twitter.com/dev_rolle"
-                                className="flex items-center gap-2"
-                            >
-                                <BsTwitter /> @dev_rolle
-                            </a>
-                            <p className="flex items-center gap-2">
-                                <MdOutlineEmail /> contact.support@ocelist.com
-                            </p>
+                        <h3>Have a repo in mind?</h3>
+                        <p>
+                            If you have a repository you’d like to see listed,
+                            whether it’s one you enjoy or your very own
+                            repository, feel free to contact me using any of the
+                            contact methods listed below. I’m always looking for
+                            great projects to add to the platform, and its
+                            completely free!
+                        </p>
+                        <div id="contacts">
+                            <div>
+                                <p>Twitter</p>
+                                <p>@dev_rolle</p>
+                            </div>
+                            <div>
+                                <p>Email</p>
+                                <p>contact.support@ocelist.com</p>
+                            </div>
                         </div>
                     </div>
                 </section>
@@ -81,3 +67,37 @@ export default function Home() {
         </>
     );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const projects = await get(ref(database, '/projects'))
+        .then((snapshot) => {
+            if (snapshot.exists()) {
+                return Object.values(snapshot.val());
+            } else {
+                return [];
+            }
+        })
+        .catch((error) => {
+            console.error(`${error.code} - ${error.message}`);
+            return [];
+        });
+
+    const highlightedProjects: any[] = [];
+
+    for (let i = 0; i < 6; i++) {
+        const index = Math.floor(Math.random() * projects.length);
+        if (
+            !highlightedProjects.includes(
+                highlightedProjects.push(projects[index])
+            )
+        ) {
+            highlightedProjects.push(projects[index]);
+        }
+    }
+
+    return {
+        props: {
+            highlightedProjects,
+        },
+    };
+};
